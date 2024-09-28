923e1598bfd2b50a6d8c92711565556c4091bbbb
Bartek Makowski <biuro@mackode.pl>
2024-09-28 10:30:34 +0000

    Add language support

---
+++ dago.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"go/scanner"
+	"os"
+	"os/user"
+	"path"
+	"regexp"
+	"strconv"
+
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
+)
+
+func mon() string {
+  usr, err := user.Current()
+  if err != nil {
+    panic(err)
+  }
+
+  logf := path.Join(usr.HomeDir, "data/monlog.txt")
+  file, err := os.Open(logf)
+  if err != nil {
+    panic(err)
+  }
+  defer file.Close()
+
+  scanner := bufio.NewScanner(file)
+  var lastLine string
+  for scanner.Scan() {
+    lastLine = scanner.Text()
+  }
+  if err := scanner.Err(); err != nil {
+    panic(err)
+  }
+
+  re := regexp.MustCompile(`\d+`)
+  match := re.FindString(lastLine)
+  n, err := strconv.ParseInt(match, 10, 64)
+  if err != nil {
+    panic(err)
+  }
+
+  n = n / 1000
+  p := message.NewPrinter(language.English)
+  return p.Sprintf("%d", n)
+}
